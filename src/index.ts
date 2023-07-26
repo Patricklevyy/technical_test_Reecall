a6dce3ec098de6344a8aa34d09af539f866ea9bf
N'da gnanmienlie patrick-levy <gnanmienlie-patrick-levy.n-da@epitech.eu>
2023-07-26 14:36:26 +0200

    [FIX] issues on the retrieving of the contacts

--- src/index.ts
+++ src/index.ts
@@ -9,18 +9,20 @@ const apiKey = 'pat-na1-e9a27d01-43f8-4e0c-b158-192a5d0cf71c';
 app.use(express.json());
 
 // Récupération de la liste des contacts
-app.get('/contacts', async (_req: Request, res: Response) => {
-  try {
-    const response = await axios.get(
-      'https://api.hubspot.com/crm/v3/objects/contacts',
-      {
-        params: { hapikey: apiKey }
-      }
-    );
-    res.json(response.data);
-  } catch (error) {
-    res.status(500).json({ error: 'Error when retrieving contacts.'});
-  }
+app.get('/contacts', async (req: Request, res: Response) => {
+    try {
+      const response = await axios.get(
+        'https://api.hubspot.com/crm/v3/objects/contacts',
+        {
+          headers: {
+            Authorization: `Bearer ${apiKey}`
+          }
+        }
+      );
+      res.json(response.data);
+    } catch (error) {
+      res.status(500).json({ error: 'Error when retrieving contacts.' });
+    }
 });
 
 // Récupération d'un contact spécifique par son ID
@@ -30,12 +32,15 @@ app.get('/contacts/:id', async (req: Request, res: Response) => {
       const response = await axios.get(
         `https://api.hubspot.com/crm/v3/objects/contacts/${contactId}`,
         {
-          params: { hapikey: apiKey }
+        //   params: { hapikey: apiKey },
+          headers: {
+            Authorization: `Bearer ${apiKey}`
+          }
         }
       );
       res.json(response.data);
     } catch (error) {
-      res.status(500).json({error: 'Error when retrieving contact by id.'});
+      res.status(500).json({ error: 'Error when retrieving contact by id.' });
     }
 });
 
@@ -47,51 +52,59 @@ app.post('/contacts', async (req: Request, res: Response) => {
         'https://api.hubspot.com/crm/v3/objects/contacts',
         newContact,
         {
-          params: { hapikey: apiKey }
+          params: { hapikey: apiKey },
+          headers: {
+            Authorization: `Bearer ${apiKey}`
+          }
         }
       );
       res.json(response.data);
     } catch (error) {
-      res.status(500).json({error: 'Error during contact creation.'});
+      res.status(500).json({ error: 'Error during contact creation.' });
     }
 });
 
 // Mise à jour d'un contact existant
 app.put('/contacts/:id', async (req: Request, res: Response) => {
-  const contactId = req.params.id;
-  const updatedContact = req.body;
-  try {
-    const response = await axios.put(
-      `https://api.hubspot.com/crm/v3/objects/contacts/${contactId}`,
-      updatedContact,
-      {
-        params: { hapikey: apiKey }
-      }
-    );
-    res.json(response.data);
-  } catch (error) {
-    res.status(500).json({ error: 'Error while updating contact.'});
-  }
+    const contactId = req.params.id;
+    const updatedContact = req.body;
+    try {
+      const response = await axios.put(
+        `https://api.hubspot.com/crm/v3/objects/contacts/${contactId}`,
+        updatedContact,
+        {
+          params: { hapikey: apiKey },
+          headers: {
+            Authorization: `Bearer ${apiKey}`
+          }
+        }
+      );
+      res.json(response.data);
+    } catch (error) {
+      res.status(500).json({ error: 'Error while updating contact.' });
+    }
 });
 
 // Suppression d'un contact
 app.delete('/contacts/:id', async (req: Request, res: Response) => {
-  const contactId = req.params.id;
-  try {
-    const response = await axios.delete(
-      `https://api.hubspot.com/crm/v3/objects/contacts/${contactId}`,
-      {
-        params: { hapikey: apiKey }
-      }
-    );
-    res.json(response.data);
-  } catch (error) {
-    res.status(500).json({ error: 'Error while deleting contact.'});
-  }
+    const contactId = req.params.id;
+    try {
+      const response = await axios.delete(
+        `https://api.hubspot.com/crm/v3/objects/contacts/${contactId}`,
+        {
+          params: { hapikey: apiKey },
+          headers: {
+            Authorization: `Bearer ${apiKey}`
+          }
+        }
+      );
+      res.json(response.data);
+    } catch (error) {
+      res.status(500).json({ error: 'Error while deleting contact.' });
+    }
 });
 
 const port = 3000;
 app.listen(port, () => {
   console.log(`App listening on port http://localhost:${port}`);
 });
-
