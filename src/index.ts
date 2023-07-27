5f117708fde90f8e7dbf84b9729e9b5484edfe97
N'da gnanmienlie patrick-levy <gnanmienlie-patrick-levy.n-da@epitech.eu>
2023-07-27 03:19:24 +0200

    [UPD] index.ts to mount the application

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,16 @@ import express, { Request, Response } from 'express';
 
 import companiesApp from './companies';
 import contactsApp from './contacts';
+import ticketsApp from './tickets';
+import transactionsApp from './transactions';
 
 const app = express();
 
+// Montage des sous-applications
 app.use('/companies', companiesApp);
 app.use('/contacts', contactsApp);
+app.use('/tickets', ticketsApp);
+app.use('/deals', transactionsApp);
 
 const port = 3000;
 app.listen(port, () => {
