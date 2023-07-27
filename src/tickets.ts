d8f4cd850c15031b78981429e7885ccdcb7ec2b5
N'da gnanmienlie patrick-levy <gnanmienlie-patrick-levy.n-da@epitech.eu>
2023-07-27 02:59:36 +0200

    [ADD] tickets.ts file for the handling of the tickets management

---
+++ src/tickets.ts
@@ -0,0 +1,100 @@
+// Import d'express et d'axios
+import express, { Request, Response } from 'express';
+import axios from 'axios';
+
+const app = express();
+const apiKey = 'pat-na1-e9a27d01-43f8-4e0c-b158-192a5d0cf71c';
+
+// Middleware pour parser les données JSON
+app.use(express.json());
+
+// Récuparation de la liste des tickets
+app.get('/', async (req: Request, res: Response) => {
+  try {
+    const response = await axios.get(`https://api.hubspot.com/crm/v3/objects/tickets`, {
+      headers: {
+        Authorization: `Bearer ${apiKey}`,
+      },
+    });
+
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: "An error occurred while retrieving the tickets." });
+  }
+});
+
+// Récupération d'un ticket spécifique par son ID
+app.get('/:id', async (req: Request, res: Response) => {
+  const ticketId = req.params.id;
+  try {
+    const response = await axios.get(`https://api.hubspot.com/crm/v3/objects/tickets/${ticketId}`, {
+      headers: {
+        Authorization: `Bearer ${apiKey}`,
+      },
+    });
+
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: "An error occurred when retrieving the ticket by ID." });
+  }
+});
+
+// Création d'un nouveau ticket
+app.post('/', async (req: Request, res: Response) => {
+  const newTicket = {
+      properties: {
+        title: req.body.properties.title,
+        subject: req.body.properties.subject,
+        priority: req.body.properties.priority,
+        category: req.body.properties.category,
+        content: req.body.properties.content,
+        requester: req.body.properties.requester
+      }
+  }
+  try {
+    const response = await axios.post(`https://api.hubspot.com/crm/v3/objects/tickets`, newTicket, {
+      headers: {
+        Authorization: `Bearer ${apiKey}`,
+      },
+    });
+
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: "An error occurred when creating a new ticket." });
+  }
+});
+
+// Mise à jour d'un ticket existant
+app.put('/:id', async (req: Request, res: Response) => {
+  const ticketId = req.params.id;
+  const updatedTicketData = req.body;
+  try {
+    const response = await axios.put(`https://api.hubspot.com/crm/v3/objects/tickets/${ticketId}`, updatedTicketData, {
+      headers: {
+        Authorization: `Bearer ${apiKey}`,
+      },
+    });
+
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: "An error occurred while updating the ticket." });
+  }
+});
+
+// Suppression d'un ticket
+app.delete('/:id', async (req: Request, res: Response) => {
+  const ticketId = req.params.id;
+  try {
+    const response = await axios.delete(`https://api.hubspot.com/crm/v3/objects/tickets/${ticketId}`, {
+      headers: {
+        Authorization: `Bearer ${apiKey}`,
+      },
+    });
+
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: "An error occurred when deleting the ticket." });
+  }
+});
+
+export default app;
